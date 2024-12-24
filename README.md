31921e0c2cf01de536ef1b31aeae01b85071632f
Thalysson Leite <thalyssonl17@gmail.com>
2024-12-24 11:08:07 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso.dev
+
 Projeto que visa reproduzir o https://www.tabnews.com.br/ para fins didáticos usando o https://curso.dev como guia.
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-	return (
-		<h1>
-			Débora, meu amor, eu te amo. Obrigado por me ajudar a me tornar a pessoa
-			que eu quero ser!
-		</h1>
-	);
+  return (
+    <h1>
+      Débora, meu amor, eu te amo. Obrigado por me ajudar a me tornar a pessoa
+      que eu quero ser!
+    </h1>
+  );
 }
 
 export default Home;
