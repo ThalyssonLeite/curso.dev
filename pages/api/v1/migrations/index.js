6bc6b6afe15c8b067b2bc6171256b38bfef198b0
Thalysson Leite <thalyssonl17@gmail.com>
2024-12-29 04:00:34 -0300

    fix `/api/v1/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,31 +3,39 @@ import migrationsRunner from "node-pg-migrate";
 import path from "node:path";
 
 export default async (request, response) => {
-	const dbClient = await database.getNewClient();
-	const defaultMigrationsOptions = {
-		dbClient,
-		dryRun: true,
-		dir: path.join("infra", "migrations"),
-		direction: "up",
-		verbose: true,
-		migrationsTable: "pgmigrations",
-	};
-	if (request.method === "GET") {
-		const pendingMigrations = await migrationsRunner(defaultMigrationsOptions);
-		await dbClient.end();
-		return response.status(200).json(pendingMigrations);
-	}
+	const allowedMethods = ["GET", "POST"];
+	if (!allowedMethods.inlcudes(request.method))
+		return response.status(405).end();
+	let dbClient;
+	try {
+		dbClient = await database.getNewClient();
+		const defaultMigrationsOptions = {
+			dbClient,
+			dryRun: true,
+			dir: path.join("infra", "migrations"),
+			direction: "up",
+			verbose: true,
+			migrationsTable: "pgmigrations",
+		};
+		if (request.method === "GET") {
+			const pendingMigrations = await migrationsRunner(
+				defaultMigrationsOptions,
+			);
+			return response.status(200).json(pendingMigrations);
+		}
 
-	if (request.method === "POST") {
-		const migratedMigrations = await migrationsRunner({
-			...defaultMigrationsOptions,
-			dryRun: false,
-		});
+		if (request.method === "POST") {
+			const migratedMigrations = await migrationsRunner({
+				...defaultMigrationsOptions,
+				dryRun: false,
+			});
+			const statusCode = migratedMigrations.length ? 201 : 200;
+			return response.status(statusCode).json(migratedMigrations);
+		}
+	} catch (error) {
+		console.error(error);
+		throw error;
+	} finally {
 		await dbClient.end();
-		const statusCode = migratedMigrations.length ? 201 : 200;
-		return response.status(statusCode).json(migratedMigrations);
 	}
-
-	await dbClient.end();
-	return response.status(405).end();
 };
